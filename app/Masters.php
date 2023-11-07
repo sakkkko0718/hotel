af354f8a9bf1834fb1fd3ed3e3a278569555147a
sakkkko0718 <sa-tofu882@ezweb.ne.jp>
2023-11-07 16:05:39 +0900

    モデルを追加

--- app/Masters.php
+++ app/Masters.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Masters extends Model
 {
-    //
+    protected $guarded = array('master_id');
+
+    public static $rules = array(
+        'room_name' => 'required',
+        'imitation' => 'required',
+    );
 }
