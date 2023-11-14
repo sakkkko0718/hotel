3183798216eef63cec4c2092362fad081eeb198b
sakkkko0718 <sa-tofu882@ezweb.ne.jp>
2023-11-14 13:47:13 +0900

    変更

--- app/Detail.php
+++ app/Detail.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Detail extends Model
 {
-    
+    protected $primarykey = 'detail_id';
+
+    public function guest(){
+        return $this->belongsTo('App\Guest','guest_id','guest_id');
+    }
+
+    public function room(){
+        //中間テーブル、中間テーブル上の相手のモデルの外部キー、中間テーブル上の自分のモデルの外部キー
+        return $this->belongsToMany('App\Room','details','reservations_id','room_id');
+    }
 }
